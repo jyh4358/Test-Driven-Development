66ae174a9ad798e624a8db63f38b69ef569266ec
jyh4358 <jyh4358@gmail.com>
2022-09-24 20:17:19 +0900

    [Membership 등록] 테스트가 실패하는 코드 작성

--- src/test/java/tdd/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/tdd/membership/repository/MembershipRepositoryTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import tdd.membership.model.Membership;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,5 +17,24 @@ public class MembershipRepositoryTest {
     @Test
     public void MembershipRepository가Null이아님() {
         assertThat(membershipRepository).isNotNull();
+    }
+
+    @Test
+    public void 멤버십등록() {
+        // given
+        final Membership membership = Membership.builder()
+                .userId("userId")
+                .membershipName("네이버")
+                .point(10000)
+                .build();
+
+        // when
+        final Membership result = membershipRepository.save(membership);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getUserId()).isEqualTo("userId");
+        assertThat(result.getMembershipName()).isEqualTo("네이버");
+        assertThat(result.getpoint()).isEqualTo(10000);
     }
 }
