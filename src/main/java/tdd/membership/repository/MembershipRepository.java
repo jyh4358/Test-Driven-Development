38e060daf2d0bc582729db2c3394daea312a5337
jyh4358 <jyh4358@gmail.com>
2022-09-24 20:40:12 +0900

    [Membership 조회] 멤버십이 존재하는지 성공 테스트 코드 작성
    MembershipRepository에 findByUserIdAndMembershipType 메서드 추가

--- src/main/java/tdd/membership/repository/MembershipRepository.java
+++ src/main/java/tdd/membership/repository/MembershipRepository.java
@@ -2,6 +2,8 @@ package tdd.membership.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tdd.membership.model.Membership;
+import tdd.membership.model.MembershipType;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+    Membership findByUserIdAndMembershipType(final String userId, final MembershipType naver);
 }
