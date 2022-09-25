e186882cbc78d0955b1161669b83f18020ec0b4d
jyh4358 <jyh4358@gmail.com>
2022-09-25 20:42:55 +0900

    [Service] 멤버십 삭제 - 테스트 실패 코드
    - 멤버십이 존재하지 않을 경우에 삭제 시 예외
    - 본인이 아닐 경우 삭제 시 예외
    - 멤버십 삭제 성공

--- src/test/java/tdd/membership/service/MembershipServiceTest.java
+++ src/test/java/tdd/membership/service/MembershipServiceTest.java
@@ -122,6 +122,44 @@ public class MembershipServiceTest {
         assertThat(result.getPoint()).isEqualTo(point);
     }
 
+    @Test
+    public void 멤버십삭제실패_존재하지않음() {
+        // given
+        doReturn(Optional.empty()).when(membershipRepository).findById(membershipId);
+
+        // when
+        final MembershipException result = assertThrows(MembershipException.class,
+                () -> target.removeMembership(membershipId, userId));
+
+        // then
+        assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.MEMBERSHIP_NOT_FOUND);
+    }
+
+    @Test
+    public void 멤버십삭제실패_본인이아님() {
+        // given
+        doReturn(Optional.of(membership())).when(membershipRepository).findById(membershipId);
+
+        // when
+        final MembershipException result = assertThrows(MembershipException.class,
+                () -> target.removeMembership(membershipId, "notOwner"));
+
+        // then
+        assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.NOT_MEMBERSHIP_OWNER);
+    }
+
+    @Test
+    public void 멤버십삭제성공() {
+        // given
+        final Membership membership = membership();
+        doReturn(Optional.of(membership)).when(membershipRepository).findById(membershipId);
+
+        // when
+        target.removeMembership(membershipId, userId);
+
+        // then
+    }
+
 
 
 
