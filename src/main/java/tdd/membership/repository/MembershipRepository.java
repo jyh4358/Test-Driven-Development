4f3a1269921252465ac5ae6d55bbc70d2141b99f
jyh4358 <jyh4358@gmail.com>
2022-09-25 15:12:40 +0900

    [Repository] 멤버십 전체 조회 - 테스트 성공 코드
    - MembershipRepository에 findAllByUserId 메서드 추가

--- src/main/java/tdd/membership/repository/MembershipRepository.java
+++ src/main/java/tdd/membership/repository/MembershipRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tdd.membership.model.Membership;
 import tdd.membership.model.MembershipType;
 
+import java.util.List;
+
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
     Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
+
+    List<Membership> findAllByUserId(String userId);
 }
