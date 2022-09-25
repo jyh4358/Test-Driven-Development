f068e06955c6466cb98e775ef11e9902f01f94b1
jyh4358 <jyh4358@gmail.com>
2022-09-25 21:48:34 +0900

    [Service] 포인트 적립 계산 테스트 성공 코드
    - 삼각 측량

--- src/main/java/tdd/membership/app/point/service/RatePointService.java
+++ src/main/java/tdd/membership/app/point/service/RatePointService.java
@@ -1,9 +1,14 @@
 package tdd.membership.app.point.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RatePointService implements PointService{
+
+    private static final int POINT_RATE = 1;
 
     @Override
     public int calculateAmount(int price) {
-        return 100;
+        return price * POINT_RATE / 100;
     }
 }
--- src/test/java/tdd/membership/app/point/service/PointServiceTest.java
+++ src/test/java/tdd/membership/app/point/service/PointServiceTest.java
@@ -24,4 +24,28 @@ public class PointServiceTest {
         // then
         assertThat(result).isEqualTo(100);
     }
+
+    @Test
+    public void _20000원의적립은200원() {
+        // given
+        final int price = 20000;
+
+        // when
+        final int result = ratePointService.calculateAmount(price);
+
+        // then
+        assertThat(result).isEqualTo(200);
+    }
+
+    @Test
+    public void _30000원의적립은300원() {
+        // given
+        final int price = 30000;
+
+        // when
+        final int result = ratePointService.calculateAmount(price);
+
+        // then
+        assertThat(result).isEqualTo(300);
+    }
 }
